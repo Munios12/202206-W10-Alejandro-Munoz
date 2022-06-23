3ba4f223b9ca834ec2ec34f458d602ec780e4da7
Alejandro Muñoz <alex12munozgeo@hotmail.es>
2022-06-23 12:18:53 +0200

    add change

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
 import { Controller } from "./components/Controller";
-import { Gentleman } from "./components/Gentleman";
 import { GentlemanList } from "./components/GentlemanList";
 import { gentlemanData } from "./data/gentlemanData";
 
